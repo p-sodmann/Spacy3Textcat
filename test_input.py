d868e5d8070d11732d9aee75f7882bf36ca3cf7d
Phil <psodmann@gmail.com>
2020-11-15 11:04:46 +0100

    small changes on the inference script

--- test_input.py
+++ test_input.py
@@ -1,13 +1,13 @@
 import spacy
-
+# load thebest model from training
 nlp = spacy.load("output/model-best")
 text = ""
-
 print("type : 'quit' to exit")
-
+# predict the sentiment until someone writes quit
 while text != "quit":
     text = input("Please enter example input: ")
-
     doc = nlp(text)
-
-    print(doc.cats["positive"])
+    if doc.cats['positive'] >.5:
+        print(f"the sentiment is positive")
+    else:
+        print(f"the sentiment is negative")
